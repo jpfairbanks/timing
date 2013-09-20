0f604066e4bdc0826e7e10cc80a93bd892b8873f
James Fairbanks <james.fairbanks@gatech.edu>
2013-09-20 09:47:30 -0400

    adding a MapTiming type for more useful output. Untestested

--- array_timing.go
+++ array_timing.go
@@ -11,69 +11,62 @@ import (
 	"time"
 )
 
-type tcollection map[string]time.Time
-type dcollection map[string]time.Duration
-
 //Timing: This is a struct for capturing performance numbers.
 //Use Tic and Toc to start and stop the timer.
-type MapTiming struct {
-	ts     tcollection
-	te     tcollection
-	Td     dcollection
+type Timing struct {
+	ts     []time.Time
+	te     []time.Time
+	Td     []time.Duration
 	length int
 }
 
 //New: create a new Timer
-func NewMapTiming(n int) MapTiming {
-	ts := make(tcollection, n)
-	te := make(tcollection, n)
-	Td := make(dcollection, n)
-	tg := MapTiming{ts, te, Td, n}
+func New(n int) Timing {
+	ts := make([]time.Time, n)
+	te := make([]time.Time, n)
+	Td := make([]time.Duration, n)
+	tg := Timing{ts, te, Td, n}
 	return tg
 }
 
 //Tic: Start the ith timer.
-func (tg MapTiming) Tic(i string) {
+func (tg Timing) Tic(i int) {
 	tg.ts[i] = time.Now()
 }
 
 //Toc: Stop the ith timer.
-func (tg MapTiming) Toc(i string) {
+func (tg Timing) Toc(i int) {
 	tg.te[i] = time.Now()
 }
 
 //Resolve: compute the deltas all at once.
-func (tg MapTiming) Resolve() {
-	for key, _ := range tg.Td {
-		tg.Td[key] = tg.te[key].Sub(tg.ts[key])
+func (tg Timing) Resolve() {
+	for i := 0; i < tg.length; i++ {
+		tg.Td[i] = tg.te[i].Sub(tg.ts[i])
 	}
 }
 
 //String: Basic representation of the deltas.
-func (tg MapTiming) String() string {
+func (tg Timing) String() string {
 	arr := make([]string, len(tg.Td))
-	i := 0
-	for _, value := range tg.Td {
-		arr[i] = fmt.Sprintf("%d", value)
-		i++
+	for i, t := range tg.Td {
+		arr[i] = fmt.Sprintf("%d", t)
 	}
 	str := strings.Join(arr, " ")
 	return str
 }
 
 //KeyString: Json friendly representation of the deltas.
-func (tg MapTiming) KeyString(key string) string {
+func (tg Timing) KeyString(key string) string {
 	return fmt.Sprintf("%s:%d,", key, tg.Td)
 }
 
 //TupleString: Columnar representation of the deltas for printing to a file.
-func (tg MapTiming) TupleString(sep string) string {
+func (tg Timing) TupleString(sep string) string {
 	var lines []string
 	lines = make([]string, tg.length)
-	i := 0
-	for key, dura := range tg.Td {
-		lines[i] = fmt.Sprintf("%d %v", key, dura.Nanoseconds())
-		i++
+	for index, dura := range tg.Td {
+		lines[index] = fmt.Sprintf("%d %v", index, dura.Nanoseconds())
 	}
 	bigstring := strings.Join(lines, sep)
 	return bigstring
--- timing_test.go
+++ timing_test.go
@@ -7,48 +7,47 @@ Liscence: BSD
 package timing
 
 import (
+	"errors"
+	"fmt"
+	"math"
+	"strings"
 	"testing"
 	"time"
-	"fmt"
-	"errors"
-	"strings"
-	"math"
 )
 
 //validate: Make sure that the timer is valid
 //not necessarily correct. this is useful for tests.
 func (tg Timing) validate() error {
 	var err error
-	if tg.length != len(tg.ts){
+	if tg.length != len(tg.ts) {
 		err = errors.New("ts is wrong length")
 	}
-	if tg.length != len(tg.te){
+	if tg.length != len(tg.te) {
 		err = errors.New("te is wrong length")
 	}
-	if tg.length != len(tg.Td){
+	if tg.length != len(tg.Td) {
 		err = errors.New("Td is wrong length")
 	}
-	for i, _ := range tg.te{
-		if tg.te[i].Sub(tg.ts[i]) < 0{
+	for i, _ := range tg.te {
+		if tg.te[i].Sub(tg.ts[i]) < 0 {
 			err = errors.New("time travel detected")
 		}
 	}
 	return err
 }
 
-
-func TestTime(t * testing.T) {
-	n:=5
+func TestTime(t *testing.T) {
+	n := 5
 	indexer := make([]int, n)
 	var err error
-	for i:=0; i<n; i++{
+	for i := 0; i < n; i++ {
 		indexer[i] = i
 	}
 	tg := New(n)
-	for i,_ := range indexer{
+	for i, _ := range indexer {
 		tg.Tic(i)
 	}
-	for i,_ := range indexer{
+	for i, _ := range indexer {
 		tg.Toc(i)
 	}
 	tg.Resolve()
@@ -58,18 +57,18 @@ func TestTime(t * testing.T) {
 	}
 }
 
-func TestValidate(t * testing.T) {
-	n:=5
+func TestValidate(t *testing.T) {
+	n := 5
 	indexer := make([]int, n)
 	var err error
-	for i:=0; i<n; i++{
+	for i := 0; i < n; i++ {
 		indexer[i] = i
 	}
 	tg := New(n)
-	for i,_ := range indexer{
+	for i, _ := range indexer {
 		tg.Tic(i)
 	}
-	for i,_ := range indexer{
+	for i, _ := range indexer {
 		tg.Toc(i)
 	}
 	var epoch time.Time
@@ -82,13 +81,13 @@ func TestValidate(t * testing.T) {
 	}
 	tg.Toc(0)
 	err = tg.validate()
-	if err != nil{
+	if err != nil {
 		t.Log("false positive")
 		t.Error(err)
 	}
-	tg.Tic(n-1)
+	tg.Tic(n - 1)
 	err = tg.validate()
-	if err == nil{
+	if err == nil {
 		t.Log("reticking")
 		t.Error(err)
 	}
@@ -97,15 +96,15 @@ func TestValidate(t * testing.T) {
 //dummyTiming: Make a timer that has the known answers
 //It should not valid. It is for testing the string representations.
 func dummyTiming() Timing {
-	n:=5
+	n := 5
 	tg := New(n)
-	for i:=0; i<n; i++{
-		tg.Td[i] = time.Duration(i)*time.Second
+	for i := 0; i < n; i++ {
+		tg.Td[i] = time.Duration(i) * time.Second
 	}
 	return tg
 }
 
-func TestString(t *testing.T){
+func TestString(t *testing.T) {
 	tg := dummyTiming()
 	out := tg.String()
 	answer := "0 1000000000 2000000000 3000000000 4000000000"
@@ -115,7 +114,7 @@ func TestString(t *testing.T){
 	}
 }
 
-func TestKeyString(t *testing.T){
+func TestKeyString(t *testing.T) {
 	tg := dummyTiming()
 	out := tg.KeyString("testkey")
 	answer := "testkey:[0 1000000000 2000000000 3000000000 4000000000],"
@@ -125,7 +124,7 @@ func TestKeyString(t *testing.T){
 	}
 }
 
-func TestTupleString(t *testing.T){
+func TestTupleString(t *testing.T) {
 	tg := dummyTiming()
 	out := tg.TupleString("\n")
 	answer := "0 0\n1 1000000000\n2 2000000000\n3 3000000000\n4 4000000000"
@@ -135,17 +134,17 @@ func TestTupleString(t *testing.T){
 	}
 }
 
-func TestResolution(t *testing.T){
+func TestResolution(t *testing.T) {
 	var count int64
 	var i int64
-	count = 100000
+	count = 10000
 	var tstart time.Time
-	var tend   time.Time
-	var td     int64
-	var tsum   int64
+	var tend time.Time
+	var td int64
+	var tsum int64
 	var sumsqr int64
 	tdarr := make([]int64, count)
-	for i = 0; i < count; i++{
+	for i = 0; i < count; i++ {
 		tstart = time.Now()
 		tend = time.Now()
 		td = tend.Sub(tstart).Nanoseconds()
@@ -154,12 +153,45 @@ func TestResolution(t *testing.T){
 		tsum += td
 		sumsqr += td * td
 	}
-	mean := tsum/count
+	mean := tsum / count
 	variance := (sumsqr/(count) - mean*mean)
 	vari := float64(0)
-	for i=0; i < count; i++{
+	for i = 0; i < count; i++ {
 		x := float64(tdarr[i]) - float64(mean)
-		vari += x*x
+		vari += x * x
+	}
+	fmt.Printf("mean: %v\n", mean)
+	fmt.Printf("variance: %v\n", variance)
+	fmt.Printf("stddev: %v\n", math.Sqrt(float64(variance)))
+	//fmt.Println(tdarr)
+}
+
+func TestAccuracy(t *testing.T) {
+	var count int64
+	var i int64
+	count = 100
+	var tstart time.Time
+	var tend time.Time
+	var td int64
+	var tsum int64
+	var sumsqr int64
+	tdarr := make([]int64, count)
+	for i = 0; i < count; i++ {
+		tstart = time.Now()
+		time.Sleep(1 * time.Second)
+		tend = time.Now()
+		td = tend.Sub(tstart).Nanoseconds()
+		//fmt.Printf("%d ", td)
+		tdarr[i] = td
+		tsum += td
+		sumsqr += td * td
+	}
+	mean := tsum / count
+	variance := (sumsqr/(count) - mean*mean)
+	vari := float64(0)
+	for i = 0; i < count; i++ {
+		x := float64(tdarr[i]) - float64(mean)
+		vari += x * x
 	}
 	fmt.Printf("mean: %v\n", mean)
 	fmt.Printf("variance: %v\n", variance)
